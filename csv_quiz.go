72801e0af05ce398ea07f489101684cda8cf722b
Yoni Melman <yoni@albert.ai>
2020-06-13 19:26:39 +0300

    Add duration to game

--- csv_quiz.go
+++ csv_quiz.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"time"
 )
 
 type question struct {
@@ -13,22 +14,21 @@ type question struct {
 	Answer   int
 }
 
-func (q question) Ask() int {
+func (q question) Ask(c chan int) {
 	var r int
-	score := 0
-	fmt.Println(q.Question, "?")
+	fmt.Printf("%v? ", q.Question)
 	_, err := fmt.Scan(&r)
 	if err != nil {
 		fmt.Println(err)
 	}
 	if q.Answer == r {
-		score++
+		c <- 1
+	} else {
+		c <- 0
 	}
-	return score
-
 }
 
-func bulidQuestion(r []string) question {
+func newQuestion(r []string) question {
 	ans, err := strconv.Atoi(r[1])
 	if err != nil {
 		fmt.Println(err)
@@ -36,20 +36,41 @@ func bulidQuestion(r []string) question {
 	return question{Question: r[0], Answer: ans}
 }
 
+func runGame(r *csv.Reader, d int) int {
+	c := make(chan int)
+	timeout := time.After(time.Duration(d) * time.Second)
+	totalScore := 0
+
+	for {
+		rec, err := r.Read()
+		if err == io.EOF { // no more questions
+			return totalScore
+		}
+		q := newQuestion(rec)
+		go q.Ask(c)
+
+		select {
+		case score := <-c:
+			{
+				totalScore += score
+			}
+		case <-timeout:
+			{
+				fmt.Println("Time out!")
+				return totalScore
+			}
+		}
+	}
+}
+
 func main() {
 	f, err := os.Open("quiz.csv")
 	if err != nil {
-		fmt.Println("something went wrong")
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	r := csv.NewReader(f)
-	score := 0
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		q := bulidQuestion(record)
-		score += q.Ask()
-	}
+	duration := 10 // seconds
+	score := runGame(r, duration)
 	println("total score: ", score)
 }
